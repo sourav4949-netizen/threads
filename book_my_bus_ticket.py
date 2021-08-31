d3e770d098572f7adb4a48b0312fdaddf2fd7d80
sourav4949 <46929852+sourav4949@users.noreply.github.com>
2021-08-31 17:43:27 +0530

    adding some more functionality

--- book_my_bus_ticket.py
+++ book_my_bus_ticket.py
@@ -1,16 +1,24 @@
 from threading import *
 
 class BookMyBus:
-    def buy(self):
-        print(current_thread().getName())
-        print("Confirming a seat")
-        print("processing a payment")
-        print("printing the ticket")
+    def __init__(self,availableSeats):
+        self.availableSeats=availableSeats
 
-obj = BookMyBus()
-t1=Thread(target=obj.buy)
-t2=Thread(target=obj.buy)
-t3=Thread(target=obj.buy)
+    def buy(self,seatRequest):
+        print(current_thread().getName())
+        print("Total seats available:",self.availableSeats)
+        if self.availableSeats>=seatRequest:
+            print("Confirming a seat")
+            print("processing a payment")
+            print("printing the ticket")
+            self.availableSeats-=seatRequest
+        else:
+            print("Requested seats are not available")
+
+obj = BookMyBus(20)
+t1=Thread(target=obj.buy,args=(3,))
+t2=Thread(target=obj.buy,args=(4,))
+t3=Thread(target=obj.buy,args=(15,))
 t1.start()
 t2.start()
 t3.start()
