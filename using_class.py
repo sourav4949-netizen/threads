1c2277dfe60f4074fd3c1951ef6b817a118d01c4
sourav4949 <46929852+sourav4949@users.noreply.github.com>
2021-08-31 16:30:46 +0530

    Creating multiple threads using class

--- using_class.py
+++ using_class.py
@@ -1,15 +1,23 @@
 from threading import *
 
+
 class MyThread:
 
     def displayNumbers(self):
-        i=0
+        i = 0
         print(current_thread().getName())
-        while i<=10:
+        while i <= 10:
             print(i)
-            i+=1
+            i += 1
 
-obj =MyThread()
-print(current_thread().getName())
-t= Thread(target=obj.displayNumbers)
+
+obj = MyThread()
+#print(current_thread().getName())
+t = Thread(target=obj.displayNumbers)
 t.start()
+
+t2 = Thread(target=obj.displayNumbers)
+t2.start()
+
+t3 = Thread(target=obj.displayNumbers)
+t3.start()
